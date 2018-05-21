0a398907ffae3955d744b83fda8fe5355cb16dc3
JeffreyTan <jeffrey.tan@123rf.com>
2018-05-21 12:04:51 +0800

    Added test class

---
+++ app/src/main/java/com/example/country/countrypicker/TestClass.java
@@ -0,0 +1,19 @@
+package com.example.country.countrypicker;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+/**
+ * Created by 123rf on 21/05/2018.
+ */
+
+public class TestClass extends LinearLayout{
+    public TestClass(Context context){
+        super(context);
+    }
+
+    public TestClass(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
