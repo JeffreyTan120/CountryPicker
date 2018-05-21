540808bfdb1c9aa4e6db8268be60474aed9504ff
JeffreyTan <jeffrey.tan@123rf.com>
2018-05-21 14:55:04 +0800

    display class

--- app/src/main/java/com/example/country/countrypicker/MainActivity.java
+++ app/src/main/java/com/example/country/countrypicker/MainActivity.java
@@ -14,6 +14,4 @@ public class MainActivity extends AppCompatActivity {
         setContentView(v);
 //        setContentView(R.layout.activity_main);
     }
-
-
 }
---
+++ app/src/main/java/com/example/country/countrypicker/PleaseDisplay.java
@@ -0,0 +1,11 @@
+package com.example.country.countrypicker;
+
+/**
+ * Created by 123rf on 21/05/2018.
+ */
+
+public class PleaseDisplay {
+    private PleaseDisplay(){
+        
+    }
+}
